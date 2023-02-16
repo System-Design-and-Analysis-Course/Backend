e18e9b8438889a41b66c144cd4c0adeb84d744ca
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-02-16 18:33:04 +0330

    panel migration files

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 from user.models import Customer
+from panel.models import CustomDate, Attachment, File, SharedFile, Library
 
 # Register your models here.
 
 admin.site.register(Customer)
+admin.site.register(CustomDate)
+admin.site.register(Library)
+admin.site.register(Attachment)
+admin.site.register(File)
+admin.site.register(SharedFile)
