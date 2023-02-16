7f9bd0bfafbe4f8cb1cf3780c1e2099fc29fbf43
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-02-16 18:18:09 +0330

    feat: add models panel

---
+++ panel/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from user.models import Customer
+
+VIDEO = 'Video'
+AUDIO = 'Audio'
+BOOK = 'Book'
+
+CONTENTTYPE_CHOICES = (
+    (VIDEO, _('Video')),
+    (AUDIO, _('Audio')),
+    (BOOK, _('Book')),
+)
+
+
+class CustomDate(models.Model):
+    created_at = models.DateTimeField(_("date created"), default=timezone.now)
+    last_modified_at = models.DateTimeField(_("date modified"), default=timezone.now)
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=50)
+    content_type = models.CharField(
+        max_length=10, choices=CONTENTTYPE_CHOICES, default=BOOK,
+        verbose_name=_('ContentType'),
+    )
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_info = models.ForeignKey(CustomDate, on_delete=models.PROTECT, null=True, blank=True)
+
+    class Meta:
+        unique_together = ('name', 'content_type', 'user',)
+
+
+class File(models.Model):
+    name = models.CharField(max_length=50)
+    content_type = models.CharField(
+        max_length=10, choices=CONTENTTYPE_CHOICES, default=BOOK,
+        verbose_name=_('ContentType'),
+    )
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_info = models.ForeignKey(CustomDate, on_delete=models.PROTECT, null=True, blank=True)
+    libraries = models.ManyToManyField(Library, null=True, blank=True)
+    content = models.FileField()
+
+    class Meta:
+        unique_together = ('name', 'content_type', 'user',)
+
+
+class Attachment:
+    name = models.CharField(max_length=50)
+    date_info = models.ForeignKey(CustomDate, on_delete=models.PROTECT, null=True, blank=True)
+    content = models.FileField(null=True, blank=True)
+    value = models.TextField(max_length=500, null=True, blank=True)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('file', 'name',)
+
+
+class SharedFile:
+    sender = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('sender', 'receiver', 'file',)
