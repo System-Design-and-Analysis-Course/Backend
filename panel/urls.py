1c04759298f8f68de9f6e209003d491ee79bca45
amirsadraabdollahi <abdollahiamirsadra@gmail.com>
2023-03-03 15:04:23 +0330

    feat: add file viewset

--- panel/urls.py
+++ panel/urls.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
 
-from panel.views import LibraryViewSet
-
+from panel.views import LibraryViewSet, FileViewSet
 app_name = 'panel'
 
 urlpatterns = [
@@ -11,4 +10,7 @@ urlpatterns = [
     re_path(r'^(?P<user_id>[0-9]+)/library/(?:(?P<pk>[0-9]+)/)?$',
             LibraryViewSet.as_view({'post': 'create', 'get': 'retrieve', 'patch': 'update', 'delete': 'destroy'}),
             name='library'),
+    re_path(r'^(?P<user_id>[0-9]+)/library/(?P<library_id>[0-9]+)/file/(?:(?P<pk>[0-9]+)/)?$',
+            FileViewSet.as_view({'post': 'create', 'get': 'retrieve', 'delete': 'destroy'}),
+            name='file'),
 ]
